932ad85ad3e118345c90b8747855704187e375e2
felipepepe <felipepepe@gmail.com>
2022-06-07 20:24:29 +0200

    Setup de descarga aplicación test velocidad en raspberry

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt install python3 python3-pip -y
+pip install mysql-connector-python
+wget https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-armhf.tgz
+tar -xvf ookla-speedtest-1.1.1-linux-armhf.tgz
+./speedtest
+
+echo "Everything installed"
